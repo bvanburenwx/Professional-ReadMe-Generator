d86150137ad634bebe3297599a88840ce736e4cf
BVanBuren <bvanburenwx@gmail.com>
2021-08-08 19:46:11 -0500

    Added the questions for inquirer to ask to generate the ReadMe file.

--- Develop/index.js
+++ Develop/index.js
@@ -4,10 +4,57 @@ const fs = require('fs');
 const generateMarkdown = require("./utils.generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please input project title.',
+    },
+    {
+        type: 'input',
+        name: 'description', 
+        message: 'Please input project description.',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'How is your project installed?',
+    },
+    {
+        type: 'input',
+        name: 'usage', 
+        message: 'How is this project used?',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Please list the contributors to this project.',
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How is your project tested?',
+    },
+    {
+        type: 'input', 
+        name: 'license',
+        message: 'Enter licenses for your project.',
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Enter your GitHub username.',
+    },
+    {
+        type: 'input', 
+        name: 'Email', 
+        message: 'Enter email address',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+}
 
 // TODO: Create a function to initialize app
 function init() {}
