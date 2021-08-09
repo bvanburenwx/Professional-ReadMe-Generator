84bfc42072033e3e4a9323e7acf4bb3a1747f26d
BVanBuren <bvanburenwx@gmail.com>
2021-08-08 20:00:48 -0500

    questions show up in the prompt for the user. Once the questions are answered. the program generates a ReadMe.md file.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require("./utils.generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -54,10 +54,21 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if(err) {
+            console.err(error);
+        }
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            writeToFile("ReadMe.md", generateMarkdown(response));
+        });
+}
 
 // Function call to initialize app
 init();
